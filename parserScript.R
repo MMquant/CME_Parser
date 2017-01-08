3360a32b1d8f9fd2194c53f3e2df776ea83e9887
Petr Javorik <MMquant@users.noreply.github.com>
2017-01-08 19:11:24 +0100

    Update parserScript.R

--- parserScript.R
+++ parserScript.R
@@ -1,7 +1,6 @@
 ### Parser script
 source("Parser.R")
 p <- Parser$new()
-p$set("db", list("192.168.88.202", 5432, "cauneasi54Ahoj"))
-write("10/28/15","log/reportdate.log")
+p$set("db", list("192.168.88.202", 5432, "yourPassword"))
 p$parse()
 p$exportQuery()
