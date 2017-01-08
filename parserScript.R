c1ca0d2c61c8cccd657bf9f61e000d1da6427f51
Petr Javorik <MMquant@users.noreply.github.com>
2017-01-08 22:10:30 +0100

    Update parserScript.R

--- parserScript.R
+++ parserScript.R
@@ -6,7 +6,7 @@ p$set("db", list("192.168.88.202", 5432, "yourPassword"))
 p$parse()
 p$exportQuery()
 # Configuring database connection
-p$set("db", list("192.168.88.202", 5432, "cauneasi54Ahoj"))
+p$set("db", list("192.168.88.202", 5432, "yourPassword"))
 # write("10/28/15","log/reportdate.log") # uncomment for debugging purposes
 # parse method of Parser object downloads and parse settlement report files
 p$parse()
