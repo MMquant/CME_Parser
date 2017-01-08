5ed5606788e5efb34547b8a481014c6f6c06b415
Petr Javorik <jvr23@linuxmail.org>
2017-01-08 22:09:50 +0100

    Update

--- parserScript.R
+++ parserScript.R
@@ -2,11 +2,9 @@
 source("Parser.R")
 # Initialization of new R6 Class object
 p <- Parser$new()
-<<<<<<< Updated upstream
 p$set("db", list("192.168.88.202", 5432, "yourPassword"))
 p$parse()
 p$exportQuery()
-=======
 # Configuring database connection
 p$set("db", list("192.168.88.202", 5432, "cauneasi54Ahoj"))
 # write("10/28/15","log/reportdate.log") # uncomment for debugging purposes
@@ -14,4 +12,3 @@ p$set("db", list("192.168.88.202", 5432, "cauneasi54Ahoj"))
 p$parse()
 # exportQuery method constructs SQL query and exports data to the database
 p$exportQuery()
->>>>>>> Stashed changes
